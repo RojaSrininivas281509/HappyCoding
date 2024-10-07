f14fa75fec940259756df59dea3817f86b46e28d
RojaSrinivas281509 <rojasrinivas281509@gmail.com>
2024-10-07 22:41:48 +0530

    Added Todolist form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+  const[tasks, setTasks] = useState([]);
+  const[newTask, setNewTask] =useState('');
+  const[error, setError] =useState('');
+  const addTask = () => {
+    if (newTask.trim() === '') {
+      setError('Task cannot be empty');
+      return;
+    }
 
-export default App;
+    const taskExists = tasks.some((task) => task.text.toLowerCase() === newTask.toLowerCase().trim());
+    if (taskExists) {
+      setError('Task already exists!');
+    } else {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+      setError(''); // Clear error message after successful addition
+    }
+  };
+
+  function toggleClicked(index){
+    const updatedTask= tasks.map((item,i)=> i===index ? {...item, completed:!item.completed}:item);
+    setTasks(updatedTask);
+  }
+  function toggleDeleteClicked(index){
+    const updatedTask= tasks.filter((item,i)=> i!==index );
+    setTasks(updatedTask);
+  }
+
+  return(
+    <div className='App'>
+      <div className='input-container'>
+        <h1>To-Do List</h1>
+        <input type='text'  placeholder='Add New Task' value={newTask} onChange={(e)=>setNewTask(e.target.value)}></input>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div>
+      {error && <p className="error-message">{error}</p>}
+      </div>
+      <ul>
+        {tasks && tasks.map((item, index)=>
+          <li key={index} className={item.completed? 'completed':''}>
+            <span onClick={()=>toggleClicked(index)}>{item.text}</span>
+            <button onClick={()=>toggleDeleteClicked(index)}>Delete</button>
+          </li>
+
+        )}
+      </ul>
+    </div>
+  )
+}
